e429b996029b35ff05b5eba6d5aabb0ba90787c8
Prachi <prachilalwani.sofmen@gmail.com>
2022-07-31 12:27:28 +0530

    Script

---
+++ DbChanges/Create dbo.Movies.sql
@@ -0,0 +1,19 @@
+﻿USE [DeltaXAssignmentContext-41f31aff-aa0a-4fd7-9b12-00229c2a92fb]
+GO
+
+/****** Object: Table [dbo].[Movies] Script Date: 07/31/2022 12:24:50 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Movies] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [MovieName]   NVARCHAR (100) NOT NULL,
+    [ReleaseDate] DATETIME2 (7)  NOT NULL,
+    [Actors]      NVARCHAR (100) NOT NULL,
+    [Producers]   NVARCHAR (100) NOT NULL
+);
+
+
---
+++ DbChanges/Create dbo.__EFMigrationsHistory_1.sql
@@ -0,0 +1,16 @@
+﻿USE [DeltaXAssignmentContext-41f31aff-aa0a-4fd7-9b12-00229c2a92fb]
+GO
+
+/****** Object: Table [dbo].[__EFMigrationsHistory] Script Date: 07/31/2022 12:24:11 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[__EFMigrationsHistory] (
+    [MigrationId]    NVARCHAR (150) NOT NULL,
+    [ProductVersion] NVARCHAR (32)  NOT NULL
+);
+
+
